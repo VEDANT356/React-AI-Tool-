25430918f735251e6c69979bd5e07d46082615b4
Vedant Kotkar <vedantkotkar2607@gmail.com>
2026-06-08 13:43:39 +0530

    Added Gemini AI chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { GoogleGenAI } from "@google/genai";
 import './App.css'
+import Answer from './components/Answers';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [question, setQuestion] = useState('');
+  const [result, setResult] = useState([]);
+
+  const ai = new GoogleGenAI({
+    apiKey: import.meta.env.VITE_GEMINI_API_KEY,
+  });
+
+  const askQuestion = async () => {
+    try {
+      const response = await ai.models.generateContent({
+        model: "gemini-3.5-flash",
+        contents: question,
+      });
+
+      setResult([...result, response.text]);
+      setQuestion('');
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>
-    </div>
+    <div className='grid grid-cols-5 h-screen text-center'>
+      <div className='col-span-1 bg-zinc-800'></div>
 
-    <div>
-    </div>
+      <div className='col-span-4'>
+        <div className='container h-110 text-white p-4 overflow-y-auto'>
 
-    <div>
-    </div>
+          {
+            result && result.map((item, index) => (
+              <Answer ans={item} key={index} />
+            ))
+          }
 
-  )
+        </div>
+
+        <div className='bg-zinc-800 w-1/2 p-1 text-white m-auto rounded-4xl border border-zinc-700 flex h-16'>
+          <input
+            type="text"
+            value={question}
+            onChange={(event) => setQuestion(event.target.value)}
+            className='w-full h-full p-3 outline-none'
+            placeholder='Ask me Anything...'
+          />
+
+          <button onClick={askQuestion} className='px-4'>
+            Ask
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Answers.jsx
@@ -0,0 +1,9 @@
+const Answer = ({ ans }) => {
+  return (
+    <div className="text-white p-4">
+      {ans}
+    </div>
+  );
+};
+
+export default Answer;
